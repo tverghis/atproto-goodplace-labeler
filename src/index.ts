c8bb035a1ffea8d4c395d50923ef6bc961ddc430
Tarun Verghis <tarun.verghis@gmail.com>
2024-11-25 23:52:49 -0800

    Drink from the firehose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+import { Event, Firehose } from "@atproto/sync";
+import { IdResolver } from "@atproto/identity";
+
+const handleEvent = async (event: Event) => {
+    if (event.event == "create" && event.record["$type"] == "app.bsky.feed.post") {
+        console.log(`${event.uri.host}: ${event.record.text}`);
+    }
+}
+
+const idResolver = new IdResolver();
+const firehose = new Firehose({
+    idResolver,
+    handleEvent,
+    onError: (_err: Error) => { }
+});
+
+
+firehose.start();
