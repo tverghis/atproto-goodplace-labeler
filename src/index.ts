789581da8b9c9a66f6faecc9c4d9e917fe640eab
Tarun Verghis <tarun.verghis@gmail.com>
2024-11-26 21:11:58 -0800

    Score posts & store in Valkey

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Event, Firehose } from "@atproto/sync";
 import { IdResolver } from "@atproto/identity";
+import { Redis } from "ioredis";
+
+const redis = new Redis();
 
 const handleEvent = async (event: Event) => {
     if (event.event == "create" && event.record["$type"] == "app.bsky.feed.post") {
-        console.log(`${event.uri.host}: ${event.record.text}`);
+        const score = randScore(-100, 100);
+        const did = event.uri.host;
+
+        await redis.incrby(userScoreKey(did), score);
+        console.log(`Gave ${did} ${score} points`);
     }
+}
+
+const randScore = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const userScoreKey = (did: string): string => {
+    return `${did}_score`;
 }
 
 const idResolver = new IdResolver();
